a330544fef3b7498d427e7faf322504eebfbd713
salman nausher <sallu4548@gmail.com>
2016-06-26 04:12:52 +0500

    mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ActionMailer::Base
-  default from: "salmanfasttian@gmail.com"
+  #default from: "salmanfasttian@gmail.com"
   def welcome_email(user)
     @user = user
     # I am overriding the 'to' default
-    mail(to: @user.email, subject: 'Do you have any spam?')
+    mail(to: 'salmanfasttian@gmail.com,shoaib.iqbal07@gmail.com', subject: 'Do you have any spam?')
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,14 +5,8 @@ TestApp::Application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
 # ActionMailer Config
-config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-config.action_mailer.delivery_method = :smtp
-# change to true to allow email to be sent during development
-config.action_mailer.perform_deliveries = false
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default :charset => "utf-8"
 
-  
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -31,8 +25,24 @@ config.action_mailer.default :charset => "utf-8"
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load
 
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.delivery_method = :smtp
+# change to true to allow email to be sent during development
+config.action_mailer.perform_deliveries = false
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            => "shoaib.iqbal07@gmail.com",
+      :password             => "pakistani314",
+      :authentication       => :login,
+      :enable_starttls_auto => true
+}
 end
